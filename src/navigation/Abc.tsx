36e25b8b823e5d4f02b65f57db82be18b977e166
Avinash Singh <avinashsingh888555@gmail.com>
2024-10-18 18:50:42 +0530

    randome changes

---
+++ src/navigation/Abc.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const Abc = (props: Props) => {
+  return (
+    <View>
+      <Text>Abc</Text>
+    </View>
+  );
+};
+
+export default Abc;
